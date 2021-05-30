e04d4f010f6cb1e284f419d370a7e1d45cd5a5d0
そのメイキー <79545730+ItsmeikyXSec4O4@users.noreply.github.com>
2021-05-30 09:54:27 +0700

    Create xvideodetail.js

---
+++ xvideodetail.js
@@ -0,0 +1,9 @@
+case 'xnxxsearch':
+anu = await fetchJson(`https://bx-hunter.herokuapp.com/api/xvideosearch?query=japan&apikey=ROOT`, {method: 'get'})
+if (anu.error) return reply(anu.error)
+teks = '────────────\n\n'
+for (let i of anu.result) {
+teks += `❏ *Title* : ${i.title}\n❏ *Info* : ${i.info}\n❏ *Link* : ${i.link}\n\n────────────\n\n`
+}
+reply(teks.trim())
+break
